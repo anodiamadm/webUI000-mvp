00b4f9e2bea041dfed3325535a808cf70665a171
unknown <anodiam.adm@gmail.com>
2021-12-02 09:40:14 +1100

    commit 2/12/2021
    commit

--- src/components/InputFields/AnodiamDropdown.js
+++ src/components/InputFields/AnodiamDropdown.js
@@ -4,12 +4,7 @@ import { AuthContext } from "../../contexts/AuthContext";
 const AnodiamDropdown = ({item, setItem, listUrl, setError}) => {
   const { auth } = useContext(AuthContext);
   const [items, setItems] = useState(null);
-  // [ { id: 1, board_short_name: 'ICSE', board_name: 'Indian Certificate of Secondary Education'},
-  //   { id: 2, board_short_name: 'ISC', board_name: 'Indian School Certificate'},
-  //   { id: 3, board_short_name: 'CBSE', board_name: 'Central Board of Secondary Education'},
-  //   { id: 4, board_short_name: 'Other', board_name: 'Other'}]
   useEffect(() => {
-    console.log("Bearer " + auth);
     fetch(listUrl, {
       headers: { 'Authorization': "Bearer " + auth }
     }).then(res => {
@@ -19,20 +14,26 @@ const AnodiamDropdown = ({item, setItem, listUrl, setError}) => {
       return res.json();
     }).then(itemsReturned => {
       setItems(itemsReturned);
-      console.log("items: ", itemsReturned);
     }).catch(err => {
       setError(err.message);
     });
-  }, [])
+  }, [auth, listUrl, setError])
   return (
     <div>
     <p>{auth}</p>
     <p>{listUrl}</p>
+    {items.map(item=>(
+      <div key={item.boardId}>value = {item.boardId}&nbsp;&nbsp;Show = {item.boardShortName}</div>
+    ))}
+
     <select className="form-control" value={item}
       onChange={(e) => setItem(e.target.value)} >
+      {items.map(item=>(
+        <option value = {item.boardId}>{item.boardShortName}</option>
+      ))}
+      <option value="1">1234</option>
       <option value="A">ABCD</option>
       <option value="X">WXYZ</option>
-      <option value="1">1234</option>
     </select>
     </div>
   );
